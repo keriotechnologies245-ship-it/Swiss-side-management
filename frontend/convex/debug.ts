9148491396ba197484d525157118692d4839332d
Roy Chumba <roychumba16@gmail.com>
2026-04-22 18:11:49 +0300

    SECURITY: Removed temporary debug file

--- frontend/convex/debug.ts
+++
@@ -1,8 +0,0 @@
-import { query } from "./_generated/server";
-
-export const debugUsers = query({
-  handler: async (ctx) => {
-    const users = await ctx.db.query("users").collect();
-    return users.map(u => ({ email: u.email, role: u.role, hasToken: !!u.token }));
-  }
-});
